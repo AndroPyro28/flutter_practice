2bf7d445595788e29c38fc34cbfa1bfc57fb82e5
Andro <menandroeugenio1028@gmail.com>
2024-05-17 10:02:20 +0800

    starter project

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home:new Test()
+  ));
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Hello World"),
+        title: new Text("Hello World"),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(31, 238, 164, 164),
       ),
-      body: const Center(
-        child: Text(
-          "Hello ninjas",
-          style: TextStyle(
-              color: Colors.blueAccent,
-              fontFamily: "IndieFlower",
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              letterSpacing: 2.0,
-              ),
-              
-        ),
+      body: Center(
+        // child: Image(
+        //   image: AssetImage('assets/toji.jpg')
+        //   // image: NetworkImage('https://images.unsplash.com/photo-1715454969547-a69afff2a701?q=80&w=1881&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+        // )
+        child: Image.network('https://images.unsplash.com/photo-1715454969547-a69afff2a701?q=80&w=1881&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {},    
         child: const Text(
-          "Click me",
+          "click me",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Color.fromARGB(255, 223, 125, 240),
-      ),
-    ),
-  ));
+        backgroundColor: Color.fromARGB(255, 223, 125, 240),  
+        shape: CircleBorder(),
+      ),  
+    );
+  }
 }
